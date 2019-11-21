6e7a8a322b3c85e67268fb36ba3822f720e08a85
NiweXY <doubletwicevik@gmail.com>
2019-11-21 20:27:54 +0200

    first

---
+++ WindowsFormsApplication6/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace WindowsFormsApplication6
+{
+    partial class Меню
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Меню));
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(26, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Каталог автомобілів";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(26, 59);
+            this.button4.MaximumSize = new System.Drawing.Size(104, 39);
+            this.button4.MinimumSize = new System.Drawing.Size(104, 39);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(104, 39);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Пошук";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(26, 209);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(104, 28);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Вихід";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(26, 118);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(104, 28);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Про автора";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Меню
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(419, 264);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(435, 302);
+            this.MinimumSize = new System.Drawing.Size(435, 302);
+            this.Name = "Меню";
+            this.Text = "Початкове меню";
+            this.Load += new System.EventHandler(this.Меню_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ WindowsFormsApplication6/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+
+namespace WindowsFormsApplication6
+{
+    public partial class Меню : Form
+    {
+        public Меню()
+        {
+            InitializeComponent();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+     /*  private void button1_Click(object sender, EventArgs e)
+        {
+
+            Form2 secondForm = new Form2();
+            secondForm.Show();
+        }*/
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           // Form3 secondForm = new Form3();
+         //  secondForm.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+           Form4 secondForm = new Form4();
+           secondForm.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Form5 secondForm = new Form5();
+            secondForm.Show();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Form6 secondForm = new Form6();
+            secondForm.Show();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            //System.Diagnostics.Process.Start(@"C:\Users\Oksana\Documents\Visual Studio 2010\Projects\kurs\Каталог автомобілів.docx");
+            /*Form7 secondForm = new Form7();
+            secondForm.Show();*/
+        }
+
+        private void Меню_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
